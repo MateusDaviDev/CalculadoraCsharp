87a5f4eedb86a26e676eade76e63b03d1f72dbc3
MateusDaviDev <mateusdavi.silva@hotmail.com>
2023-06-03 22:15:59 -0300

    first commit

---
+++ CalculadoraCmd/Operacoes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraCmd
+{
+    class Operacoes
+    {
+       public void Soma()
+        {
+            Console.WriteLine("Soma de dois numeros: ");
+            Console.WriteLine("Digite o primeiro numero:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo numero:");
+            float b = float.Parse(Console.ReadLine());
+            float resultado = a + b;
+            Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+
+
+        }
+
+       public void Subtracao()
+        {
+            Console.WriteLine("Subtracao de dois numeros: ");
+            Console.WriteLine("Digite o primeiro numero:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo numero:");
+            float b = float.Parse(Console.ReadLine());
+            float resultado = a - b;
+            Console.WriteLine("O resultado da Subtracao é: " + resultado);
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+
+        }
+
+        public void Divisao()
+        {
+            Console.WriteLine("Divisao de dois numeros: ");
+            Console.WriteLine("Digite o primeiro numero:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo numero:");
+            float b = float.Parse(Console.ReadLine());
+            float resultado = a / b;
+            Console.WriteLine("O resultado da Divisao é: " + resultado);
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+
+        }
+
+        public void Multiplicacao()
+        {
+            Console.WriteLine("Multiplicacao de dois numeros: ");
+            Console.WriteLine("Digite o primeiro numero:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo numero:");
+            float b = float.Parse(Console.ReadLine());
+            float resultado = a * b;
+            Console.WriteLine("O resultado da Divisao é: " + resultado);
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+
+        }
+
+        public void Potencia()
+        {
+            Console.WriteLine("Potencia de dois numeros: ");
+            Console.WriteLine("Digite o primeiro numero:");
+            float baseNum = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo numero:");
+            float expo = float.Parse(Console.ReadLine());
+            double resultado = Math.Pow(baseNum, expo);
+            Console.WriteLine("O resultado da Divisao é: " + resultado);
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+
+        }
+
+        public void RaizQuadrada()
+        {
+            Console.WriteLine("Raiz de um  numeros: ");
+            float raizNum = float.Parse(Console.ReadLine());
+            double resultado = Math.Sqrt(raizNum);
+            Console.WriteLine("O resultado da Raiz de " + raizNum + " é: " + resultado.ToString("F"));
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CalculadoraCmd/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CalculadoraCmd
+{
+    class Program
+    {
+        enum Menu
+        {
+            Soma=1, Subtracao = 2, Divisao = 3, Multiplicacao = 4, Potencia = 5, Raiz = 6, Sair = 7
+        }
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+            while (!escolheuSair) 
+            {
+                Console.WriteLine("Seja bem vindo a calculadora, selecione uma das opções");
+                Console.WriteLine("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Potência\n6-Raiz\n7-Sair");
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
+                Console.WriteLine(opcao);
+                Console.Clear();
+
+                Operacoes minhasOperacoes = new Operacoes();
+
+                switch (opcao)
+                {
+                    case Menu.Sair:
+                        escolheuSair = true;
+                        break;
+
+                    case Menu.Soma:
+                        minhasOperacoes.Soma();
+                        break;
+
+                    case Menu.Subtracao:
+                        minhasOperacoes.Subtracao();
+                        break;
+                    case Menu.Divisao:
+                        minhasOperacoes.Divisao();
+                        break;
+                    case Menu.Multiplicacao:
+                        minhasOperacoes.Multiplicacao();
+                        break;
+                    case Menu.Potencia:
+                        minhasOperacoes.Potencia();
+                        break;
+                    case Menu.Raiz:
+                        minhasOperacoes.RaizQuadrada();
+                        break;
+                }
+
+
+
+
+
+
+            } 
+
+        }
+        
+    }
+}
